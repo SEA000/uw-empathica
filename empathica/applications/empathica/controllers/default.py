335e064cf34f1015767f38faaa40a8a7be2e8794
esolodkin <esolodkin@57bd4de8-d0ae-4296-9854-44d4de5c7137>
2012-03-08 23:37:22 +0000

    - Made the update downloading mandatory
    git-svn-id: http://ctnsrv.uwaterloo.ca/svn/esolodkin/trunk/Empathica@147 57bd4de8-d0ae-4296-9854-44d4de5c7137

--- empathica/applications/empathica/controllers/default.py
+++ empathica/applications/empathica/controllers/default.py
@@ -2,7 +2,7 @@ import urllib
 
 def index():
     if settings.web2py_runtime_gae:
-        return dict(need_to_update=False)
+        return dict()
         
     try:
         opener = urllib.FancyURLopener({})
@@ -12,25 +12,12 @@ def index():
         if len(data) > 1:
             version = float(data)
             if empathica_version < version:
-                return dict(need_to_update=True, version=version)
+                uploadfolder = os.path.join(request.folder, 'uploads')
+                urllib.urlretrieve(update_donwload_url, uploadfolder + os.sep + 'update.w2p')
+                session.flash=T("Empathica update was successfully donwloaded and will be installed on next restart.")
     except:
         pass
-        
-    return dict(need_to_update=False)
-
-@onerror
-def update():    
-    if settings.web2py_runtime_gae:
-        redirect(request.wsgi.environ['HTTP_REFERER'])
-    
-    try:
-        uploadfolder = os.path.join(request.folder, 'uploads')
-        urllib.urlretrieve(update_donwload_url, uploadfolder + os.sep + 'update.w2p')
-        session.flash=T("Update was successfully donwloaded and will be installed on next restart.")
-        redirect(request.wsgi.environ['HTTP_REFERER'])
-    except Exception, e:
-        session.flash=T("The update cannot be downloaded at this time! Please, try again later.")
-        redirect(request.wsgi.environ['HTTP_REFERER'])
+    return dict()
     
 @onerror
 def about():
